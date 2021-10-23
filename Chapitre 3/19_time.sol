1d4178da3d20bc7aecf63353a7065721b8c2b98d
Yoyae <gingin.fr@gmail.com>
2021-10-23 14:55:13 +0200

    update formating time exercice bis

--- Chapitre 3/19_time.sol
+++ Chapitre 3/19_time.sol
@@ -1,1 +1,7 @@
-pragma solidity 0.8.9;contract Time {	function getTime() public view returns(uint){		return block.timestamp;	}}
+pragma solidity 0.8.9;
+
+contract Time {
+	function getTime() public view returns(uint){
+		return block.timestamp;
+	}
+}
