cd53bc09547288bd5bb1efca6564d0a22650d100
Yoyae <gingin.fr@gmail.com>
2021-10-28 03:21:18 +0200

    Adding chapter 4

---
+++ Chapitre 4/1_SimpleStorage.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.8.9;
+ 
+contract SimpleStorage {
+   uint data;
+ 
+   function set(uint x) public {
+       data = x;
+   }
+ 
+   function get() public view returns (uint) {
+       return data;
+   }
+}
