2cc73433b90c2f42eecb8e0f5c1bd189218c66ad
lecascyril <castagnet.cyril@gmail.com>
2022-03-11 17:02:22 +0100

    added selfdestruct

---
+++ Chapitre 5/40_selfDestructB.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.12;
+
+contract Wallet{
+
+	// Activation of selfdestruct at addr address
+  function activateSelfdestruct(address addr) public {
+      selfdestruct(addr);
+  }
+}
