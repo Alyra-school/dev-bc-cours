aad7f3d13c5221fb054cad542cf35ad7b0aae3a9
Castagnet Cyril <45956179+lecascyril@users.noreply.github.com>
2022-10-12 03:33:29 +0200

    Update 5_erc20.test.js

--- Chapitre 4/5_erc20.test.js
+++ Chapitre 4/5_erc20.test.js
@@ -1,43 +1,55 @@
-// erc20.test.js 
-const { BN } = require('@openzeppelin/test-helpers');
+const MyToken = artifacts.require("./MyToken.sol");
+const { BN , expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
-const ERC20 = artifacts.require('ERC20Token');
-contract('ERC20', function (accounts) {
-const _name = 'ALYRA';
-const _symbol = 'ALY';
-const _initialsupply = new BN(1000);
-const _decimals = new BN(18);
-const owner = accounts[0];
-const recipient = accounts[1];
- 
- beforeEach(async function () {
-  this.ERC20Instance = await ERC20.new(_initialsupply,{from: owner});
- });
- 
-it('a un nom', async function () {
-  expect(await this.ERC20Instance.name()).to.equal(_name);
-});
-it('a un symbole', async function () {
-  expect(await this.ERC20Instance.symbol()).to.equal(_symbol);
-});
-it('a une valeur décimal', async function () {
-  expect(await this.ERC20Instance.decimals()).to.be.bignumber.equal(_decimals);
-});
-it('vérifie la balance du propriétaire du contrat', async function (){
-  let balanceOwner = await this.ERC20Instance.balanceOf(owner);
-  let totalSupply = await this.ERC20Instance.totalSupply();
-  expect(balanceOwner).to.be.bignumber.equal(totalSupply);
-});
-it('vérifie si un transfer est bien effectué', async function (){
-  let balanceOwnerBeforeTransfer = await this.ERC20Instance.balanceOf(owner);
-  let balanceRecipientBeforeTransfer = await this.ERC20Instance.balanceOf(recipient);
-  let amount = new BN(10);
- 
-  await this.ERC20Instance.transfer(recipient, amount, {from: owner});
-  let balanceOwnerAfterTransfer = await this.ERC20Instance.balanceOf(owner);
-  let balanceRecipientAfterTransfer = await this.ERC20Instance.balanceOf(recipient);
- 
-  expect(balanceOwnerAfterTransfer).to.be.bignumber.equal(balanceOwnerBeforeTransfer.sub(amount));
-  expect(balanceRecipientAfterTransfer).to.be.bignumber.equal(balanceRecipientBeforeTransfer.add(amount));
-});
+
+
+contract("MyToken", accounts => {
+  const _name = "Alyra"
+  const _symbol = "ALY" 
+  const _initialSupply = new BN(10000);
+  const _owner = accounts[0];
+  const _recipient = accounts[1];
+  const _decimal = new BN(18);
+
+  let MyTokenInstance;
+
+  beforeEach(async function(){
+    MyTokenInstance= await MyToken.new(_initialSupply, {from: _owner});
+  });
+
+  it("has a name", async () => {
+    expect(await MyTokenInstance.name()).to.equal(_name);
+  });
+
+  it("has a symbol", async () => {
+    expect(await MyTokenInstance.symbol()).to.equal(_symbol);
+  });
+
+  it("has a decimal", async () => {
+    expect(await MyTokenInstance.decimals()).to.be.bignumber.equal(_decimal);
+  });
+
+  it("check first balance", async () => {
+    expect(await MyTokenInstance.balanceOf(_owner)).to.be.bignumber.equal(_initialSupply);
+  });
+
+  it("check balance after transfer", async () => {
+    let amount = new BN(100);
+    let balanceOwnerBeforeTransfer = await MyTokenInstance.balanceOf(_owner);
+    let balanceRecipientBeforeTransfer = await MyTokenInstance.balanceOf(_recipient)
+
+    expect(balanceRecipientBeforeTransfer).to.be.bignumber.equal(new BN(0));
+    
+    await MyTokenInstance.transfer(_recipient, new BN(100), {from: _owner});
+
+    let balanceOwnerAfterTransfer = await MyTokenInstance.balanceOf(_owner);
+    let balanceRecipientAfterTransfer = await MyTokenInstance.balanceOf(_recipient)
+
+    expect(balanceOwnerAfterTransfer).to.be.bignumber.equal(balanceOwnerBeforeTransfer.sub(amount));
+    expect(balanceRecipientAfterTransfer).to.be.bignumber.equal(balanceRecipientBeforeTransfer.add(amount));
+
+    
+  });
+
+
 });
