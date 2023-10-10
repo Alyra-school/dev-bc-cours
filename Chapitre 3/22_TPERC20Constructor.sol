7046f5114c80178747bbd41ad4c29b27f0925c6e
Castagnet Cyril <45956179+lecascyril@users.noreply.github.com>
2023-10-10 19:49:53 +0200

    Update 22_TPERC20Constructor.sol

--- Chapitre 3/22_TPERC20Constructor.sol
+++ Chapitre 3/22_TPERC20Constructor.sol
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
  
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
  
 contract ERC20Token is ERC20 {   
    constructor() ERC20("ALYRA", "ALY") {
